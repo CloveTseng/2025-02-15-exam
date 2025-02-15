7cad0f5b5d7e1289e9013d15c76af0613d430341
DolceTseng <jessica.turn613@gmail.com>
2025-02-15 15:28:32 +0800

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('https://randomuser.me/api/?results=10');
+        setData(res.data.results);
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="container mx-auto p-4">
         <div className="row">
-          <div className="col-md-4">
-            <div className="bg-light p-3">
-              <img
-                src="https://randomuser.me/api/portraits/women/61.jpg"
-                alt="頭像"
-                className="img-fluid rounded-circle"
-              />
-              <h2 className="mb-0">Mona Heen</h2>
-              <p className="mb-0">mona.heen@example.com</p>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="bg-light p-3">
-              <img
-                src="https://randomuser.me/api/portraits/women/10.jpg"
-                alt="頭像"
-                className="img-fluid rounded-circle"
-              />
-              <h2 className="mb-0">Susan Craig</h2>
-              <p className="mb-0">susan.craig@example.com</p>
-            </div>
-          </div>
+          {data.map((item) => {
+            return (
+              <div className="col-md-4" key={item.id}>
+                <div className="bg-light p-3">
+                  <img
+                    src={item.picture.large}
+                    alt="頭像"
+                    className="img-fluid rounded-circle"
+                  />
+                  <h2 className="mb-0">{item.name.title}</h2>
+                  <p className="mb-0">{item.email}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
